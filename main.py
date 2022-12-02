860d4df26ca7ae3283861d1a29af66095c82d564
0xCollab <65812901+0xCollab@users.noreply.github.com>
2022-12-02 19:55:04 +0530

    Committing the final change

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
 import requests
+import sys
+import urllib.parse
+from bs4 import BeautifulSoup
+
+
+# def validurl(str):
+#    #r = requests.get(url)
+#    if r.status_code == 200:
+#        return url
+#    else:
+#        return exit(0)
+
+# def fileopen(str):
+#    f1 = open("payload.txt","r+")
+#    for i in f1:
+#        return i
+
+def sendget(str):
+    f1 = open('payload.txt').readlines()
+    #    print (f1)
+    for line in f1:
+        #        payload = line
+        #       print(payload)
+        params = {'query': line}
+        value = urllib.parse.urlencode(params)
+        req = requests.get(str, params=params)
+        #        print(req.url)
+        #        print(req.headers)
+        #        print(req.content)
+        print(value)
+        soup = BeautifulSoup(req.text)
+        for value in doc.find_all(text=req.compile(), limit=2):
+            print(value)
+#        print(req.content)
 
 print("enter URL for scanning in https://yoururl.com format")
 url = input()
-if len(url)==0:
+if len(url) == 0:
     print("no url to scan")
-    exit(0)
 else:
-    r = requests.get(url)
-    print(r.status_code)
-    print(r.text)
+    sendget(url)
